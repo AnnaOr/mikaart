2db50ae9511cb2bffa0f3fb4d7fd8013104c4c52
Anna Orfinyak <aorfinyak@gmail.com>
2017-04-14 11:54:48 +0000

    new contact form and rewrite form.js

--- js/form.js
+++ js/form.js
@@ -1,29 +1,24 @@
-
 $(function() {
     $('#btn-submit').click(sendForm);
 });
 
 function sendForm(e) {
     var form = document.querySelector('form');
-    if (!form[0].checkValidity()) {
-        form.find(':submit').click(); // We try to submit invalid form to show message 
-    }
-    else {
+    if (form.checkValidity()) {
         e.preventDefault();
         $.ajax({
-                url: "https://formspree.io/aorfinyak@gmail.com",
-                method: "POST",
-                data: {
-                    clientName: $('#name').val(),
-                    clientEmail: $('#email').val(),
-                    
-                    message: $('#message').val()
-                },
-                dataType: "json"
-            })
-            .done(function() {
-                $('#messageDone').html('Дякую за повідомлення');
-            });
-    }
+            url: "https://formspree.io/aorfinyak@gmail.com",
+            method: "POST",
+            data: {
 
+                clientName: $('#name').val(),
+
+                clientEmail: $('#email').val(),
+                clientMessage: $('#message').val(),
+            },
+            dataType: "json"
+        }).done(function() {
+            $('#messageDone').html('Дякую за повідомлення!');
+        });
+    }
 }
